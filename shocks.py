f61a9dda6eb0ddbbff8a2211f0f9a4e6cb7bbf21
Tim Plomin <tplomin@gmail.com>
2021-06-17 22:47:08 -0500

    Shock relations

--- shocks.py
+++ shocks.py
@@ -1,4 +1,5 @@
 import numpy as np
+pi = np.pi
 
 def idealGas(R, P=0, rho=0, T=0):
     """
@@ -18,5 +19,34 @@ def speedOfSound(gamma, R, T):
     """
     return np.sqrt(gamma* R * T)
 
+def p_ratio(gamma, m1, beta = pi/2):
+    """
+    Pressure ratio across the shock
+    """
+    m1n = m1*np.sin(beta)
+    return 1 + 2 * gamma / (gamma + 1) * (m1n**2 - 1) 
+
+def rho_ratio(gamma, m1n, beta = 90):
+    """
+    Density ratio across the shock
+    """
+    return (gamma  + 1)*m1n**2 / (2 + (gamma - 1 )*m1n**2)
+
+def u_ratio(gamma, m1n, beta = 90):
+    """
+    Velocity ratio across the shock
+    """
+    return 1 / rho_ratio(gamma, m1n)
+
+def t_ratio(gamma, m1n, beta = 90):
+    """
+    Temperature ratio across the shock
+    """
+    return p_ratio(gamma, m1n) / rho_ratio(gamma, m1n)
+
+def m2n(gamma, m1n, beta = 0):
+    num = m1n**2 + 2/(gamma-1)
+    den = (2*gamma)/(gamma - 1) * m1n**2 - 1
+    return np.sqrt( num / den )
 
 
