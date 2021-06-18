ec573020f1a2ab11db2658545cd40cb4140783f2
Tim Plomin <tplomin@gmail.com>
2021-06-17 21:51:00 -0500

    Add ideal gas function

---
+++ shocks.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def idealGas(R, P=0, rho=0, T=0):
+    """
+    Solve ideal gas equations 
+    P = rho R T
+    """
+    if (P == 0):
+        return rho * R * T
+    elif (rho == 0):
+        return P / (R * T)
+    else:
+        return P / (rho * R)
+    
